e297a5811d0eb6d86a3fdd850d02f851dca71ea4
Mykhailo Soroka <mvsoroka28@gmail.com>
2019-08-19 00:39:42 +0300

    initial

---
+++ WebApplication1/Funcs/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Funcs
+{
+    public static class Extensions
+    {
+        public static Maybe<T> Return<T>(this T value) where T : class
+            => value != null
+                ? new Maybe<T>(value)
+                : Maybe<T>.None();
+    }
+}
---
+++ WebApplication1/Funcs/Maybe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WebApplication1.Funcs
+{
+    public class Maybe<TInput>
+    {
+        private readonly TInput _value;
+
+        public Maybe(TInput value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            _value = value;
+        }
+
+        private Maybe() { }
+
+        public static Maybe<TInput> None() => new Maybe<TInput>();
+
+        public Maybe<TResult> Bind<TResult>(Func<TInput, Maybe<TResult>> func) where TResult: class
+        {
+            return func != null ? func(_value) : Maybe<TResult>.None();
+        }
+    }
+}
